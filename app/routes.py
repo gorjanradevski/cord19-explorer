c38eb3e5727c58479bb3e45fcef546869ceeffd2
Gorjan Radevski <gorjanradevski@gmail.com>
2020-04-16 14:30:39 +0200

    git lfs test

--- app/routes.py
+++ app/routes.py
@@ -7,8 +7,9 @@ import torch
 from torch import nn
 from transformers import BertTokenizer
 import json
-import gdown
-import os
+
+# import gdown
+# import os
 from app.article import Article
 
 # https://stackoverflow.com/questions/59122308/heroku-slug-size-too-large-after-installing-pytorch
@@ -20,16 +21,16 @@ def index():
     form = SearchForm()
     tokenizer = BertTokenizer.from_pretrained("google/bert_uncased_L-4_H-512_A-8")
     # Download model
-    url = "https://drive.google.com/uc?id=11OHi9wETRPAHUTIH4p6BqZY3gH6NJtve"
+    # url = "https://drive.google.com/uc?id=11OHi9wETRPAHUTIH4p6BqZY3gH6NJtve"
     model_path = "app/static/models/cord_smallbert_grounding.pt"
-    if not os.path.exists(model_path):
-        gdown.download(url, model_path, quiet=True)
+    # if not os.path.exists(model_path):
+    # gdown.download(url, model_path, quiet=True)
     if form.query.data is not None:
         config = BertConfig.from_pretrained("google/bert_uncased_L-4_H-512_A-8")
         model = nn.DataParallel(
             SentenceMappingsProducer("google/bert_uncased_L-4_H-512_A-8", config, 3)
         )
-        model.load_state_dict(torch.load(model_path, map_location=torch.device("cpu"),))
+        model.load_state_dict(torch.load(model_path, map_location=torch.device("cpu")))
         model.train(False)
         with torch.no_grad():
             input_ids = torch.tensor(tokenizer.encode(form.query.data)).unsqueeze(0)
